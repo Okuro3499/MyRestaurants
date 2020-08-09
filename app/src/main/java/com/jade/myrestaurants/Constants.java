429f82b79d1d92aef6622058ce21a75754a7f2d5
Gideon Okuro <gideonollonde@gmail.com>
2020-08-09 13:58:53 +0300

    add files to be ignored

--- app/src/main/java/com/jade/myrestaurants/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.jade.myrestaurants;
-
-public interface Constants {
-    public static final String YELP_BASE_URL = "https://api.yelp.com/v3/";
-    public static final String YELP_API_KEY = BuildConfig.YELP_API_KEY;
-}
-
-
