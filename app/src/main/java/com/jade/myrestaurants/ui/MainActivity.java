0fd5831f3c6b30bf2346692628d0f42142b71877
Gideon Okuro <gideonollonde@gmail.com>
2020-08-14 14:17:26 +0300

    greet user by name

--- app/src/main/java/com/jade/myrestaurants/ui/MainActivity.java
+++ app/src/main/java/com/jade/myrestaurants/ui/MainActivity.java
@@ -59,6 +59,8 @@
 package com.jade.myrestaurants.ui;
 
 import android.content.Intent;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Typeface;
@@ -71,6 +73,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -90,11 +93,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @BindView(R.id.savedRestaurantsButton)
     Button mSavedRestaurantsButton;
 
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user!= null) {
+                    getSupportActionBar().setTitle("Welcome, " + user.getDisplayName() + "!");
+                } else {
+
+                }
+            }
+        };
 
         Typeface ostrichFont = Typeface.createFromAsset(getAssets(), "fonts/ostrich_regular.ttf");
         mAppNameTextView.setTypeface(ostrichFont);
@@ -131,4 +150,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         finish();
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if(mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
 }
